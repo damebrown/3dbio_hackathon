253a98a34d56735ae2b20971f98fa244585cf66f
Edan Patt <punims@cs.huji.ac.il>
2021-06-23 15:37:34 +0300

    working on the clustering pipeline

--- clustering.py
+++ clustering.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from kneed import KneeLocator
+# from kneed import KneeLocator
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans, DBSCAN, SpectralClustering, AgglomerativeClustering
 from sklearn.metrics import silhouette_score
@@ -10,16 +10,18 @@ from sklearn.pipeline import Pipeline
 import numpy as np
 
 
-class ClusterPipeline:
-    clustering_set = set("Kmeans", "Spectral", "DBScan", "Hierarchical")
+import matplotlib.pyplot
 
-    def __init__(self, model: str, dim_reduction: bool, scaler: str, **kwargs):
+class ClusterPipeline:
+
+    def __init__(self, model: str, dim_reduction: str, scaler: str, **kwargs):
 
         self.model = model
         self.dim_reduction = dim_reduction
         self.scaler = scaler
-        self.__dict__ = kwargs
-        self.pipeline = None
+        self.pipeline: Pipeline = None
+        self.added_dict = kwargs
+
         self._create()
 
     def init_preprocess(self) -> Pipeline:
@@ -30,22 +32,22 @@ class ClusterPipeline:
 
     def init_dim_reduction(self) -> Pipeline:
         if self.dim_reduction == "PCA":
-            return Pipeline(["PCA", PCA(2)])
+            return Pipeline([("PCA", PCA(2))])
         if self.dim_reduction == "Tsne":
-            return Pipeline(["Tsne", TSNE(2)])
+            return Pipeline([("Tsne", TSNE(2))])
 
     def init_cluster(self) -> Pipeline:
         if self.model == "Kmeans":
-            return Pipeline([("Kmeans", KMeans(**self.__dict__))])
+            return Pipeline([("Kmeans", KMeans(**self.added_dict))])
 
         if self.model == "Spectral":
-            return Pipeline([("Spectral", SpectralClustering(**self.__dict__))])
+            return Pipeline([("Spectral", SpectralClustering(**self.added_dict))])
 
         if self.model == "DBScan":
-            return Pipeline([("DBScan", DBSCAN(**self.__dict__))])
+            return Pipeline([("DBScan", DBSCAN(**self.added_dict))])
 
         if self.model == "Hierarchical":
-            return Pipeline([("Hierarchical", AgglomerativeClustering(**self.__dict__))])
+            return Pipeline([("Hierarchical", AgglomerativeClustering(**self.added_dict))])
 
     def _create(self):
         self.pipeline = Pipeline([("preprocessor", self.init_preprocess()),
@@ -58,4 +60,25 @@ class ClusterPipeline:
         :param data:
         :return:
         """
-        self.pipeline.fit(data)
+        return self.pipeline.fit_transform(data)
+
+
+
+def main():
+
+    np.random.seed(1)
+    pipe = ClusterPipeline("Kmeans", "PCA", "Standard", n_clusters=10, max_iter=500)
+    rand_data = np.random.random((10000, 10000) )
+    print(rand_data)
+    Yt = pipe.fit(rand_data)
+    Xt = (pipe.pipeline.named_steps)['dim reduction'].named_steps['PCA'].transform(rand_data)
+    labels = pipe.pipeline.named_steps["clusterer"].named_steps["Kmeans"].labels_
+    print(Xt)
+
+    plt.plot(Xt[:, 0], Xt[:, 1])
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    main()
