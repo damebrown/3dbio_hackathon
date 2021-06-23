f1d2ddb24dec1f155ea5e520f0ee29e2b3ef1c22
Edan Patt <punims@cs.huji.ac.il>
2021-06-23 16:04:59 +0300

    added plots

--- clustering.py
+++ clustering.py
@@ -64,18 +64,35 @@ class ClusterPipeline:
 
 
 
+
+
 def main():
+
+    # load data into matrix
+        # How much data can we load, if not everything how do we split it up in an intelligent way
+
+    # cluster
+        # What type of clustering algorithm works best
+        # if there's a lot of data can Kmeans / Spectral work? if we reduce the amount of data would they work?
+        # PCA or Tsne for dimensional reduction?
+
+    # plot
+        # heatmaps vs scatter plots
 
     np.random.seed(1)
     pipe = ClusterPipeline("Kmeans", "PCA", "Standard", n_clusters=10, max_iter=500)
     rand_data = np.random.random((10000, 10000) )
-    print(rand_data)
+
     Yt = pipe.fit(rand_data)
     Xt = (pipe.pipeline.named_steps)['dim reduction'].named_steps['PCA'].transform(rand_data)
     labels = pipe.pipeline.named_steps["clusterer"].named_steps["Kmeans"].labels_
-    print(Xt)
 
-    plt.plot(Xt[:, 0], Xt[:, 1])
+
+    fig, ax = plt.subplots()
+
+    for g in np.unique(labels):
+        i = np.where(labels == g)
+        ax.scatter(Xt[:, 0][i], Xt[:, 1][i], label=g)
     plt.show()
 
 
