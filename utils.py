b9d234bd0b33130540d4b2941edac0f2d09d26a6
linoytsaban_14 <linoy.tsaban@mail.huji.ac.il>
2021-06-24 11:40:17 +0300

    code to take matrix from triangular to symmetric

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 ####################################################################################################
 # Code to replace the name of the structures in the cluster files with their corresponding number  #
 ####################################################################################################
@@ -12,18 +15,34 @@ def create_num_to_name_dict(dict_path):
 
 
 def replace_names_cluster_files(clusters_path, dict_path):
-    new_file = open("replaced_"+clusters_path, 'w')
+    new_file = open("replaced_" + clusters_path, 'w')
     orig_file = open(clusters_path, 'r')
     name_to_num = create_num_to_name_dict(dict_path)
     for line in orig_file.readlines():
         if '...' in line:
             name = line.split("...")[0].split(">")[1]
-            new_line = name_to_num[name] +"\n"
+            new_line = name_to_num[name] + "\n"
             new_file.write(new_line)
         else:
             new_file.write(line)
     orig_file.close()
     new_file.close()
 
+
+def triangle_to_symmetric_matrix(matrix_path):
+    df = pd.read_csv(matrix_path, sep=',', header=None)
+    orig_matrix = df.values()
+    # orig_matrix = np.array([[123,107,113,103],[140,110,108],[136,106],[115]])
+    new_matrix = np.zeros((orig_matrix.shape[0],orig_matrix.shape[0]))
+    for i in range(orig_matrix.shape[0]):
+        left_side = [orig_matrix[k][j] for k,j in zip(range(0,i,1),range(i,0,-1))]
+        new_matrix[i] = np.array(left_side + orig_matrix[i])
+    print(new_matrix)
+    return new_matrix
+
+
+
+
 # create_num_to_name_dict("name_num_dict")
-replace_names_cluster_files("output_0.8.clstr", "data_files/name_num_dict")
+# replace_names_cluster_files("output_0.8.clstr", "name_num_dict")
+triangle_to_symmetric_matrix("")
