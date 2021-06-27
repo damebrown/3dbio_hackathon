4e3dce171d77eaae4622de8724701dc26794dafe
punims <edanpatt@gmail.com>
2021-06-27 21:18:14 +0300

    changed utils to only work with minmax standardization and now made a heatmap for max values

--- utils.py
+++ utils.py
@@ -5,17 +5,25 @@ import pandas as pd
 import numpy as np
 import preproccessing
 from preproccessing.utils import create_num_to_name_dict
-import os
-import preproccessing.preproccess as prcs
+import preproccessing.preproccess as preproccess
 import seaborn as sbn
+
+FILES = [
+    # "data_files\\labels\\representatives_1000_MinibatchKmeans_200_Tsne_2_Standard.pickle",
+    "data_files\\labels\\representatives_1000_MinibatchKmeans_200_Tsne_2_MinMax.pickle",
+    "data_files\\labels\\representatives_1000_MinibatchKmeans_200_None_2_MinMax.pickle",
+    # "data_files\\labels\\representatives_1000_MinibatchKmeans_200_None_2_Standard.pickle"
+]
+
+titles = ['None', 'Tsne']
 
 
 ####################################################################################################
 # Code to replace the name of the structures in the cluster files with their corresponding number  #
 ####################################################################################################
 
-def triangle_to_symmetric_matrix(matrix_path, dict_path=False):
-    df = pd.read_csv(matrix_path, sep=',', header=None)
+def triangle_to_symmetric_matrix(matrix_path, dict_path = False):
+    df = pd.read_csv(matrix_path, sep = ',', header = None)
     orig_matrix = df.values[:, :df.values.shape[0]]
     # orig_matrix = np.array([[123,107,113,103],[140,110,108, np.nan],[136,106, np.nan, np.nan],[115, np.nan, np.nan, np.nan]])
     new_matrix = np.zeros((orig_matrix.shape[0], orig_matrix.shape[0]))
@@ -40,17 +48,28 @@ def triangle_to_symmetric_matrix(matrix_path, dict_path=False):
 
 
 def get_key(val, dict):
+    """
+    this function gets a value and a dictionary and returns the key in which this value appears as {key: value}.
+    if {key: value} doesn't exist in dict, returns None
+    """
     for key, value in dict.items():
         if val in value:
             return key
     return None
 
 
-def func(samples):
-    alons_clusters_dict = prcs.parse_DCHit()
+def find_clusters(samples):
+    '''
+    this function recieves samples and using the dict from parse_DCHit andd the false2true_index_dict (taken from the file
+    "preproccessing/struct_to_index_second_1000.txt") finds the right index in of the sample in alons clusters dict
+    # samples is an np.array of the samples from the pickle file
+    returns samples with another column with the right key
+    '''
+    alons_clusters_dict = preproccess.parse_DCHit()
+    # false to true index (like indexes file)
     false2true_index_dict = preproccessing.utils.create_num_to_name_dict(
         "preproccessing/struct_to_index_second_1000.txt", False)
-    # false to true index (like indexes file)
+    # adding another column to samples in order to place the labels in the matrix
     samples = np.c_[samples, np.zeros(len(samples))].astype(int)
     for i, sample in enumerate(samples):
         x = int(false2true_index_dict[str(i + 1)])
@@ -73,43 +92,58 @@ def plot_heatmap(X: np.array, y: np.array, title: str, save_path: str = None):
 
 
 def plot_heatmap_max(X: np.array, title: str, save_path: str = None):
-    plt.figure()
-    sbn.heatmap(X, cmap = "YlGnBu")
+    df = pd.DataFrame({"Max": X},
+                      index=list(range(1, 201)))
+    sbn.heatmap(df, fmt="g", cmap="YlGnBu")
     plt.title(title)
     plt.savefig(save_path)
     plt.show()
 
 
-# yitzhack's clusters
-files = [
-    "/data_files/labels/representatives_1000_MinibatchKmeans_200_Tsne_2_Standard.pickle",
-    "/data_files/labels/representatives_1000_MinibatchKmeans_200_Tsne_2_MinMax.pickle",
-    "/data_files/labels/representatives_1000_MinibatchKmeans_200_None_2_MinMax.pickle",
-    "/data_files/labels/representatives_1000_MinibatchKmeans_200_None_2_Standard.pickle"]
-for k, path in enumerate(files):
-    f = open(path, 'rb')
-    file = pickle.load(f)
-    smp = np.array(file)
-    answers = func(smp).astype(int)
-    yitz_labels = np.unique(answers[:, 0])
-    alon_labels = np.unique(answers[:, 1])
+def parse_pickle_files(files):
+    '''
+    this function takes as argument a list of pathes to pickle files and then parses them and returns the average
+    value of the top probabilites for each file as a list
+    '''
+    # iterating over the files
+    probabilities_avg = []
+    for k, path in enumerate(files):
+        pickle_file = pickle.load(open(path, 'rb'))
+        np_samples = np.array(pickle_file)
+        answers = find_clusters(np_samples).astype(int)
+        yitz_labels = np.unique(answers[:, 0])
+        alon_labels = np.unique(answers[:, 1])
+        # correlation_matrix is used to count the amount of occurences of every tuple of clusters from the two
+        # different cluster methods
+        correlation_matrix = np.zeros((len(yitz_labels) + 1, len(alon_labels) + 1))
+        # adding the labels to the matrix as first row and first column
+        correlation_matrix[0] = np.array([0] + list(yitz_labels))
+        correlation_matrix[:, 0] = np.array([0] + list(alon_labels))
+        for i, y in enumerate(yitz_labels):
+            for j, a in enumerate(alon_labels):
+                # iterating over the tuples of labels
+                for ans in answers:
+                    # checking that this tupleof answers exists
+                    if ans[0] == y and ans[1] == a:
+                        # incrementing by 1 the count of this tuple
+                        correlation_matrix[i + 1, j + 1] += 1
+        # after calculating the corrolations between the two different clusterings, now we will compute the
+        # probabilites of the most frequent tuples
+        correlation_matrix = correlation_matrix.astype(int)
+        # making a copy to work on:
+        copy_dist_matrix = correlation_matrix[1:, 1:]
+        probabilites_arr = []
+        max_arr = []
+        for row in copy_dist_matrix:
+            probabilites_arr.append(row / sum(row))
+            max_arr.append(max(row / sum(row)))
+        plot_heatmap(probabilites_arr, max_arr, 'Probabilites of cluster matching, dim reduction: ' + titles[k], "data_files\\Figures\\figs" + str(k))
+        # plot_heatmap_max(max_arr, 'Maximum Probabilites of cluster matching', "data_files\Figures\\figsmax"+ str(i))
+        probabilities_avg.append(np.average(max_arr))
+    # return the average of all of the file's probabilites
+        plot_heatmap_max(np.array(max_arr), "max probability for same cluster heatmap, avg= " + str(np.average(max_arr)),"data_files\\Figures\\figs_max" + str(k))
+    return probabilities_avg
 
-    mat = np.zeros((len(yitz_labels) + 1, len(alon_labels) + 1))
-    mat[0] = np.array([0] + list(yitz_labels))
-    mat[:, 0] = np.array([0] + list(alon_labels))
-    for i, y in enumerate(yitz_labels):
-        for j, a in enumerate(alon_labels):
-            for ans in answers:
-                if ans[0] == y and ans[1] == a:
-                    mat[i + 1, j + 1] += 1
-    mat = mat.astype(int)
-    m = mat[1:, 1:]
-    arr = []
-    max_arr = []
-    for j, r in enumerate(m):
-        arr.append(r / sum(r))
-        max_arr.append(max(r / sum(r)))
-    plot_heatmap(arr, max_arr, 'Probabilites of cluster matching', "data_files\Figures\\figs" + str(k))
-    # plot_heatmap_max(max_arr, 'Maximum Probabilites of cluster matching', "data_files\Figures\\figsmax"+ str(i))
-    print(np.average(max_arr))
 
+avg = parse_pickle_files(FILES)
+print(avg)
